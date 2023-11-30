4b67f4823a0ea92d076848c5ba80697ac7999b4f
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2023-11-30 15:29:12 +0530

    redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/redirect301",
+        destination: "/hotel",
+        permanent: true,
+        statusCode: 301,
+      },
+      {
+        source: "/redirect302",
+        destination: "/hotel",
+        permanent: false,
+        statusCode: 302,
+      },
+      {
+        source: "/redirect307",
+        destination: "/hotel",
+        permanent: false,
+      },
+      {
+        source: "/redirect308",
+        destination: "/hotel",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
