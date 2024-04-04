24e88e9bc2a7a5b4c997fac422f364b87d710088
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-04-04 15:34:53 +0530

    feat: api performance

--- src/app/[language-countrycode]/api-performance/page.js
+++ src/app/[language-countrycode]/api-performance/page.js
@@ -10,7 +10,7 @@ export default function ApiPerformance() {
     const startTimeInMs = new Date().getTime();
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/todos/1",
-      { cache: "no-store" }
+      {}
     );
     const jsonResponse = await response.json();
 
@@ -39,7 +39,9 @@ export default function ApiPerformance() {
             method: "GET",
           },
           data: "",
-          headers: {},
+          headers: {
+            ...getCommonHeaders({}),
+          },
           isCritical: true,
           page: "SamplePage",
           responseHandler: "ApiPerformance",
