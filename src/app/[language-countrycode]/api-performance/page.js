fa87ae7c1e9831f0e64c109eccd59eba0bbbe3e2
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-04-05 12:02:07 +0530

    test: tiket api on webview

--- src/app/[language-countrycode]/api-performance/page.js
+++ src/app/[language-countrycode]/api-performance/page.js
@@ -4,8 +4,13 @@ import { fetch } from "../../../../lib/fetch";
 import { getCommonHeaders } from "@tiket/react-common-utilities";
 
 export default function ApiPerformance() {
+  // states for FE Fetch
   const [feFetchData, setFeFetchData] = useState(null);
+  const [feFetchTiketData, setFeFetchTiketData] = useState(null);
+
+  // states for JSI Fetch
   const [jsiFetchData, setJsiFetchData] = useState(null);
+  const [jsiFetchTiketData, setJsiFetchTiketData] = useState(null);
 
   const callApi = async () => {
     const startTimeInMs = new Date().getTime();
@@ -47,6 +52,67 @@ export default function ApiPerformance() {
     }
   };
 
+  const callTiketApi = async () => {
+    // start time
+    const startTimeInMs = new Date().getTime();
+    try {
+      const response = await fetch(
+        `https://m.gatotkaca.tiket.com/ms-gateway/tix-promotion-page/promos/ferry-test?areaSize=6&brandSize=6&category=campaign&inventorySize=6&promoCodeSize=10`,
+        {
+          opts: {
+            headers: {
+              Authorization:
+                "Bearer eyJraWQiOiJGbGpkRExkSjA3VmhNUU00clh1Nk5fMTNfX2U4ajFsayJ9.eyJhdWQiOiJ0aWtldC5jb20iLCJzdWIiOiI2NjBmODZiMjEyODFiNTY0ZjUwMGY3ZGMiLCJuYmYiOjE3MTIyOTM1NTQsImlzcyI6Imh0dHBzOi8vd3d3LnRpa2V0LmNvbSIsImV4cCI6MTcxMjg5ODM1NH0.IuWz7PXT6alVLLh29O9HQR80xAPTmCUxRfhBzNXwJRk2lx7tbWh-MAp99TL4lOvl",
+            },
+          },
+        }
+      );
+      const jsonData = await response.json();
+
+      // end time
+      const endTimeInMs = new Date().getTime();
+
+      setFeFetchTiketData(
+        `fe-fetch-tiket-api-duration: ${endTimeInMs - startTimeInMs}`
+      );
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      setFeFetchTiketData(
+        `fe-fetch-tiket-api-failed: ${JSON.stringify(error)}`
+      );
+    }
+  };
+
+  const callTiketApiJsi = async () => {
+    if (typeof window !== "undefined") {
+      // start time
+      const startTimeInMs = new Date().getTime();
+      window.JSIStartTime = startTimeInMs;
+
+      const jsiArg = {
+        command: "fetchApi",
+        request: {
+          url: `https://m.gatotkaca.tiket.com/ms-gateway/tix-promotion-page/promos/ferry-test?areaSize=6&brandSize=6&category=campaign&inventorySize=6&promoCodeSize=10`,
+          protocolConfig: {
+            config: "",
+            method: "GET",
+          },
+          data: "",
+          headers: {
+            ...getCommonHeaders({}),
+            Authorization:
+              "Bearer eyJraWQiOiJGbGpkRExkSjA3VmhNUU00clh1Nk5fMTNfX2U4ajFsayJ9.eyJhdWQiOiJ0aWtldC5jb20iLCJzdWIiOiI2NjBmODZiMjEyODFiNTY0ZjUwMGY3ZGMiLCJuYmYiOjE3MTIyOTM1NTQsImlzcyI6Imh0dHBzOi8vd3d3LnRpa2V0LmNvbSIsImV4cCI6MTcxMjg5ODM1NH0.IuWz7PXT6alVLLh29O9HQR80xAPTmCUxRfhBzNXwJRk2lx7tbWh-MAp99TL4lOvl",
+            "Cache-Control": "no-cache",
+          },
+          isCritical: true,
+          page: "SamplePageTiket",
+          responseHandler: "ApiPerformanceTiket",
+        },
+      };
+      window.generic?.callGenericNativeJSI?.(JSON.stringify(jsiArg));
+    }
+  };
+
   useEffect(() => {
     const handleNativeJSICallback = (e) => {
       const data = JSON.parse(e?.detail?.response?.data);
@@ -56,8 +122,8 @@ export default function ApiPerformance() {
       // end time
       const endTimeInMs = new Date().getTime();
 
-      setJsiFetchData(`jsi-fetch-api-duration: ${endTimeInMs - startTimeInMs}`);
       window.JSIStartTime = undefined;
+      setJsiFetchData(`jsi-fetch-api-duration: ${endTimeInMs - startTimeInMs}`);
 
       return;
     };
@@ -83,6 +149,24 @@ export default function ApiPerformance() {
       </button>
       <pre style={{ wordBreak: "break-all" }} id="api-response-jsi">
         {jsiFetchData}
+      </pre>
+
+      <div
+        style={{ width: "100%", height: "2px", background: "#42a5f2" }}
+      ></div>
+
+      <button onClick={callTiketApi} id="call-tiket-api-fe">
+        Call Tiket Api - Fe Fetch
+      </button>
+      <pre style={{ wordBreak: "break-all" }} id="api-response-tiket-fe">
+        {feFetchTiketData}
+      </pre>
+
+      <button onClick={callTiketApiJsi} id="call-tiket-api-jsi">
+        Call Tiket Api - JSI
+      </button>
+      <pre style={{ wordBreak: "break-all" }} id="api-response-tiket-jsi">
+        {jsiFetchTiketData}
       </pre>
     </>
   );
