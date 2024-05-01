ff75e0c80c37013b2f62b3b6e53b561f4f38c2af
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-05-02 01:25:53 +0530

    fix: add to calendar button

--- src/app/[language-countrycode]/remove-jsi/page.js
+++ src/app/[language-countrycode]/remove-jsi/page.js
@@ -1,45 +1,7 @@
 "use client";
+import AddToCalendarButton from "add-to-calendar-button";
 
 export default function RemoveJSI() {
-  function createICSFile(event) {
-    // Create the iCalendar file content
-    const icsContent = `
-BEGIN:VCALENDAR
-VERSION:2.0
-BEGIN:VEVENT
-SUMMARY:${event.title}
-DTSTART:${new Date(event.startDate).toISOString().replace(/-|:|\.\d+/g, "")}
-DTEND:${new Date(event.endDate).toISOString().replace(/-|:|\.\d+/g, "")}
-LOCATION:${event.location}
-DESCRIPTION:${event.description}
-END:VEVENT
-END:VCALENDAR
-`;
-
-    // Create a Blob from the iCalendar content
-    const blob = new Blob([icsContent], { type: "text/calendar" });
-    return URL.createObjectURL(blob);
-  }
-
-  function AddToCalendarButton({ event }) {
-    // Generate the download URL for the .ics file
-    const icsFileURL = createICSFile(event);
-
-    return (
-      <a href={icsFileURL} download={`${event.title}.ics`}>
-        Add to Calendar
-      </a>
-    );
-  }
-
-  const event = {
-    title: "My Event",
-    startDate: "2024-05-01T10:00:00",
-    endDate: "2024-05-01T11:00:00",
-    location: "123 Main St",
-    description: "An important event",
-  };
-
   return (
     <div>
       <button
@@ -84,21 +46,17 @@ END:VCALENDAR
         Open Map
       </button>
 
-      <button
-        onClick={() => {
-          try {
-            window.open(
-              "intent://add_event#Intent;scheme=com.android.calendar;end"
-            );
-          } catch (e) {
-            window.open("calshow://");
-          }
-        }}
-      >
-        Open Calendar and add event
-      </button>
-
-      <AddToCalendarButton event={event} />
+      <AddToCalendarButton
+        name="[Reminder] Test the Add to Calendar Button"
+        startDate="2024-05-05"
+        startTime="10:15"
+        endTime="23:30"
+        timeZone="America/Los_Angeles"
+        location="World Wide Web"
+        description="Check out the maybe easiest way to include Add to Calendar Buttons to your web projects:[br]→ [url]https://add-to-calendar-button.com/|Click here![/url]"
+        options="'Apple','Google','iCal','Outlook.com','Microsoft 365','Microsoft Teams','Yahoo'"
+        lightMode="bodyScheme"
+      />
     </div>
   );
 }
