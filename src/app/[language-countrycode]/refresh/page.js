1374fb6e7873aeb7443236131376ed778d01f1dd
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-05-24 12:56:43 +0530

    test: reload

--- src/app/[language-countrycode]/refresh/page.js
+++ src/app/[language-countrycode]/refresh/page.js
@@ -1,12 +1,33 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Refresh() {
   const router = useRouter();
+  const [loading, setIsLoading] = useState({ bool: true, key: 0 });
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [loading.key]);
+
+  if (loading.bool) {
+    return <h2>Loading</h2>;
+  }
+
   return (
     <>
       <button onClick={() => window.location.reload()}>Reload</button>
-      <button onClick={() => router.refresh()}>Router Refresh</button>
+      <button
+        onClick={() => {
+          setIsLoading({
+            bool: true,
+            key: loading.key + 1,
+          });
+          router.refresh();
+        }}
+      >
+        Router Refresh
+      </button>
     </>
   );
 }
