1d42261f8e516af3026ee0f21a0ddbfed43136d8
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-06 19:34:57 +0530

    chore: update isRedirect property and remove unnecessary comments

--- src/app/[language-countrycode]/errorscreen/page.js
+++ src/app/[language-countrycode]/errorscreen/page.js
@@ -1,10 +1,36 @@
 "use client";
-import { PageRenderPerformanceMarker } from "@tiket/react-common-jsi";
+import { PageRenderPerformanceMarker, logWebApi } from "@tiket/react-common-jsi";
 import { useEffect, useState } from "react";
 
 export default function ErrorScreen() {
   const [code, setCode] = useState(null);
   useEffect(() => {
+    logWebApi({
+      exception: "Forbidden",
+      method: "GET",
+      query: "https://something.com/api/v1/getData",
+      reqPayloadSize: 100,
+      respCode: 403,
+      respTime: 2000,
+      uri: "https://something.com/api/v1/getData",
+      businessCode: 400,
+      businessMsg: "none",
+      respPayloadSize: 20000,
+      correlationId: "1234",
+    });
+    logWebApi({
+      exception: "Forbidden",
+      method: "POST",
+      query: "https://something.com/api/v2/getData",
+      reqPayloadSize: 100,
+      respCode: 403,
+      respTime: 2000,
+      uri: "https://something.com/api/v2/getData",
+      businessCode: 400,
+      businessMsg: "none",
+      respPayloadSize: 20000,
+      correlationId: "1234",
+    });
     setTimeout(() => {
       setCode(403);
     }, 1000);
