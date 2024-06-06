5156635f4dd3e386cfbcb2d1c50c2dbb86b006cf
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-06 17:38:52 +0530

    chore: update isRedirect property and remove unnecessary comments

--- src/app/[language-countrycode]/hotel/page.js
+++ src/app/[language-countrycode]/hotel/page.js
@@ -1,8 +1,27 @@
+"use client";
 import { headers } from "next/headers";
 import { CentralRouter } from "../../../../component/CentralRouter";
+import { useEffect, useState } from "react";
+import { PageRenderPerformanceMarker } from "@tiket/react-common-jsi";
 
 export default function Hotel() {
+  const [code, setCode] = useState(null);
+  useEffect(() => {
+    setTimeout(() => {
+      setCode(200);
+    }, 2000);
+  }, []);
+
   const headersList = headers();
   const referrer = headersList.get("referer");
-  return <CentralRouter referrerHeader={referrer} />;
+  if (!code) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <CentralRouter referrerHeader={referrer} />;
+      <PageRenderPerformanceMarker respCode={code} message="Success" />
+    </>
+  );
 }
