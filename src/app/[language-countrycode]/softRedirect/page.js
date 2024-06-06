05e859ec405eeeef2c03ca6e70d0994217c08afd
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-06 17:47:48 +0530

    chore: update isRedirect property and remove unnecessary comments

--- src/app/[language-countrycode]/softRedirect/page.js
+++ src/app/[language-countrycode]/softRedirect/page.js
@@ -1,10 +1,12 @@
 "use client";
+import { onNavigationStart } from "@tiket/react-common-jsi";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function SoftRedirect({ params }) {
   const router = useRouter();
   useEffect(() => {
+    onNavigationStart({ url: "/hotel" });
     router.push(`/hotel`);
   }, []);
   return <></>;
