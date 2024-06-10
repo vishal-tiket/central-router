6fcc3d11e48ff9cbbe22272a65d4b439ba0f10db
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-10 10:04:27 +0530

    test: refund page pagevisit event

--- src/app/[language-countrycode]/not-found.js
+++ src/app/[language-countrycode]/not-found.js
@@ -1,13 +1,27 @@
 "use client";
+import { PageRenderPerformanceMarker } from "@tiket/react-common-jsi";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
   const router = useRouter();
+  const [code, setCode] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCode(404);
+    }, 1000);
+  });
+
+  if (!code) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <div>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <button onClick={() => router.back()}>Go Back</button>
+      <PageRenderPerformanceMarker respCode={code} message={"page not found"} />
     </div>
   );
 }
--- src/app/[language-countrycode]/refresh/page.js
+++ src/app/[language-countrycode]/refresh/page.js
@@ -1,16 +1,21 @@
 "use client";
+import { PageRenderPerformanceMarker } from "@tiket/react-common-jsi";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Refresh() {
   const router = useRouter();
   const [loading, setIsLoading] = useState({ bool: true, key: 0 });
+  const [code, setCode] = useState(null);
 
   useEffect(() => {
     setIsLoading(false);
+    setTimeout(() => {
+      setCode(200);
+    }, 2000);
   }, [loading.key]);
 
-  if (loading.bool) {
+  if (loading.bool || !code) {
     return <h2>Loading</h2>;
   }
 
@@ -28,6 +33,11 @@ export default function Refresh() {
       >
         Router Refresh
       </button>
+
+      <PageRenderPerformanceMarker
+        respCode={code}
+        message={"page rendered successfully"}
+      />
     </>
   );
 }
