e22925763c45880f8b93158e81ed14b39c9990a6
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-25 16:20:27 +0530

    fix: overriding global handle response method

--- component/CentralRouter/CentralRouter.js
+++ component/CentralRouter/CentralRouter.js
@@ -87,6 +87,12 @@ export const CentralRouter = ({
     (params && params?.["language-countrycode"]?.split("-")) || [];
 
   useEffect(() => {
+    // vertical side handleResponse implementation
+    window.handleResponse = () => {
+      console.log("this is vertical side handle response method !!");
+      return true;
+    };
+
     /** car properties mapping */
     window.CARProperties = {
       android: {
