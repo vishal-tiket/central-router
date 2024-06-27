f060f5ef11b9f70ea324492627fa49489a5380c1
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-06-27 07:58:42 +0530

    feat: add location

--- src/app/[language-countrycode]/permissions/page.js
+++ src/app/[language-countrycode]/permissions/page.js
@@ -3,13 +3,12 @@
 import { useRef, useState } from "react";
 
 export default function Permissions() {
-  const cameraRef = useRef(null);
-  const microphoneRef = useRef(null);
-
-  const [cameraState, setCameraState] = useState(false);
-  const [microphoneState, setMicrophoneState] = useState(false);
+  const [camera, setCamera] = useState({});
+  const [microphone, setMicrophone] = useState({});
+  const [location, setLocation] = useState({});
 
   const startCamera = async () => {
+    console.log("startCamera");
     if (
       "mediaDevices" in navigator &&
       "getUserMedia" in navigator.mediaDevices
@@ -21,14 +20,14 @@ export default function Permissions() {
           height: 220,
         },
       });
-      setCameraState(true);
-      cameraRef.current.srcObject = stream;
+      setCamera(stream);
     }
   };
   const stopCamera = async () => {
-    if (cameraRef.current.srcObject) {
-      cameraRef.current.srcObject.getTracks().forEach((track) => track.stop());
-      setCameraState(false);
+    console.log("stopCamera");
+    if (camera.getTracks) {
+      camera.getTracks().forEach((track) => track.stop());
+      setCamera({});
     }
   };
 
@@ -41,18 +40,24 @@ export default function Permissions() {
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
       });
-      setMicrophoneState(true);
-      microphoneRef.current.srcObject = stream;
+      setMicrophone(stream);
     }
   };
   const stopMicrophone = async () => {
     console.log("stopMicrophone");
-    if (microphoneRef.current.srcObject) {
-      microphoneRef.current.srcObject
-        .getTracks()
-        .forEach((track) => track.stop());
+    if (microphone.getTracks) {
+      microphone.getTracks().forEach((track) => track.stop());
 
-      setMicrophoneState(false);
+      setMicrophone({});
+    }
+  };
+
+  const getLocation = () => {
+    console.log("getLocation");
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation(position);
+      });
     }
   };
   return (
@@ -61,31 +66,38 @@ export default function Permissions() {
       <h3>Camera</h3>
       <button onClick={startCamera}>Play</button>
       <button onClick={stopCamera}>Pause</button>
-      {cameraState && (
-        <video
-          ref={cameraRef}
-          style={{
-            border: "1px solid black",
-            margin: "20px 0",
-            width: "100%",
-            height: "220px",
-          }}
-        />
-      )}
+      <video
+        src={camera}
+        style={{
+          border: "1px solid black",
+          margin: "20px 0",
+          width: "100%",
+          height: "220px",
+        }}
+      />
 
       <h3>Microphone</h3>
       <button onClick={startMicrophone}>Play</button>
       <button onClick={stopMicrophone}>Pause</button>
-      {microphoneState && (
-        <audio
-          ref={microphoneRef}
-          style={{
-            border: "1px solid black",
-            margin: "20px 0",
-            width: "100%",
-            height: "30px",
-          }}
-        />
+      <audio
+        src={microphone}
+        style={{
+          border: "1px solid black",
+          margin: "20px 0",
+          width: "100%",
+          height: "30px",
+        }}
+      />
+
+      <h3>Location</h3>
+      <button onClick={getLocation}>Get Location</button>
+      {location?.coords && (
+        <div style={{ margin: "20px 0" }}>
+          {JSON.stringify({
+            latitude: location?.coords?.latitude,
+            longitude: location?.coords?.longitude,
+          })}
+        </div>
       )}
     </>
   );
