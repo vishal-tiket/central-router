a5e85e10d56d68cef748c7e050575283a7941c36
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-07-08 17:26:24 +0530

    fix: code refactoring

--- src/app/[language-countrycode]/jsiperformance/page.js
+++ src/app/[language-countrycode]/jsiperformance/page.js
@@ -11,7 +11,10 @@ export default function JSIPerformance() {
     setIsSmallContentLoading(true);
     const res = await Share({
       files: [
-        "https://images.unsplash.com/photo-1510505678115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        {
+          url: "https://images.unsplash.com/photo-1510505678115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          name: "sssssss",
+        },
       ],
     });
     setIsSmallContentLoading(false);
@@ -21,7 +24,10 @@ export default function JSIPerformance() {
     setIsLargeContentLoading(true);
     const res = await Share({
       files: [
-        "https://drive.google.com/uc?export=download&id=1nJPkC6I2L9BfmYUSFLOhDye18878zQv8",
+        {
+          url: "https://drive.google.com/uc?export=download&id=1nJPkC6I2L9BfmYUSFLOhDye18878zQv8",
+          name: "ssss",
+        },
       ],
     });
     setIsLargeContentLoading(false);
