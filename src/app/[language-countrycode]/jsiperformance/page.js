833ad8f60d59f91688f952b13069162b483ebb7c
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-07-10 07:18:00 +0530

    fix: code refactoring

--- src/app/[language-countrycode]/jsiperformance/page.js
+++ src/app/[language-countrycode]/jsiperformance/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Share } from "@tiket/react-common-navigator-permission";
+import { ShareDownloadableFiles } from "@tiket/react-common-navigator-permission";
 import { useEffect, useState } from "react";
 
 export default function JSIPerformance() {
@@ -9,7 +9,7 @@ export default function JSIPerformance() {
 
   const callJSIWithSmallContent = async () => {
     setIsSmallContentLoading(true);
-    const res = await Share({
+    const res = await ShareDownloadableFiles({
       files: [
         {
           url: "https://images.unsplash.com/photo-1510505678115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -43,7 +43,7 @@ export default function JSIPerformance() {
 
   const callJSIWithLargeContent = async () => {
     setIsLargeContentLoading(true);
-    const res = await Share({
+    const res = await ShareDownloadableFiles({
       files: [
         {
           url: "https://drive.google.com/uc?export=download&id=1nJPkC6I2L9BfmYUSFLOhDye18878zQv8",
