8b377ec54edef6f33b09e215037a4408929956e4
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-07-15 14:51:49 +0530

    chore: update changeset

--- src/app/[language-countrycode]/jsiperformance/page.js
+++ src/app/[language-countrycode]/jsiperformance/page.js
@@ -12,10 +12,6 @@ export default function JSIPerformance() {
     try {
       const response = await ShareDownloadableFiles([
         {
-          url: "https://images.unsplash.com/photo-15105056bb78115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          name: "sssssss1",
-        },
-        {
           url: "https://images.unsplash.com/photo-1510505678115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
           name: "sssssss2",
         },
@@ -38,22 +34,17 @@ export default function JSIPerformance() {
       ]);
       console.log("dsadasdasdasdasdA", response);
     } catch (e) {
-      setMultipleFileLoading(false);
-      setError(e);
       console.log("error caught");
+    } finally {
+      setMultipleFileLoading(false);
     }
   };
 
   return (
     <div>
       <h3>Share Wrapper</h3>
-      <h4>FE Error Case</h4>
       <button onClick={shareFiles}>Share Multiple Files</button>
-      {multipleFileLoading ? (
-        <div>Loading</div>
-      ) : (
-        <div>{JSON.stringify(error)}</div>
-      )}
+      {multipleFileLoading && <div>Loading</div>}
     </div>
   );
 }
