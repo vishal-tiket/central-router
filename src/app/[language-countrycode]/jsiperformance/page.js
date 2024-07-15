ebb03c612be85c4018df788d1472a351f9c7a5e2
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-07-15 14:39:47 +0530

    chore: update changeset

--- src/app/[language-countrycode]/jsiperformance/page.js
+++ src/app/[language-countrycode]/jsiperformance/page.js
@@ -1,14 +1,13 @@
 "use client";
 
 import { ShareDownloadableFiles } from "@tiket/react-common-navigator-permission";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function JSIPerformance() {
-  const [isSmallContentLoading, setIsSmallContentLoading] = useState(false);
-  const [isLargeContentLoading, setIsLargeContentLoading] = useState(false);
+  const [multipleFileLoading, setMultipleFileLoading] = useState(false);
 
-  const callJSIWithSmallContent = async () => {
-    setIsSmallContentLoading(true);
+  const shareFiles = async () => {
+    setMultipleFileLoading(true);
     try {
       const response = await ShareDownloadableFiles([
         {
@@ -40,70 +39,14 @@ export default function JSIPerformance() {
     } catch (e) {
       console.log("error caught");
     }
-    setIsSmallContentLoading(false);
-  };
-
-  const callJSIWithLargeContent = async () => {
-    setIsLargeContentLoading(true);
-    const res = await ShareDownloadableFiles([
-      {
-        url: "https://drive.google.com/uc?export=download&id=1nJPkC6I2L9BfmYUSFLOhDye18878zQv8",
-        name: "ssss",
-      },
-    ]);
-    console.log(res);
-    setIsLargeContentLoading(false);
-  };
-
-  const download = async () => {
-    try {
-      const res = await fetch(
-        "https://images.unsplash.com/photo-15105056bb78115-f2a7ae4cfea9?q=80&w=1681&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      );
-      if (!res.ok) {
-        // Checks if status is outside 200-299
-        console.log("dasdasdasdas");
-        throw new Error(`HTTP error!: ${res.status}`);
-      }
-      console.log(res);
-    } catch (e) {
-      throw new Error(e.message);
-    }
-  };
-
-  const promiseHandler = () => {
-    try {
-      return new Promise((res, rej) => {
-        try {
-          res(undefined);
-        } catch (e) {
-          console.log("inside promise");
-          rej(e);
-        }
-      });
-    } catch (e) {
-      console.log("outside promise");
-      console.log(e);
-    }
-  };
-
-  const errorHandler = async () => {
-    try {
-      const res = await download();
-    } catch (e) {
-      console.log("outside errorHandler");
-      console.log("dasda", e);
-    }
+    setMultipleFileLoading(false);
   };
 
   return (
     <div>
-      <h3>JSI Performance</h3>
-      <button onClick={callJSIWithSmallContent}>JSI with small content</button>
-      {isSmallContentLoading && <div>Loading</div>}
-      <button onClick={callJSIWithLargeContent}>JSI with large content</button>
-      {isLargeContentLoading && <div>Loading</div>}
-      <button onClick={errorHandler}>errorHandler</button>
+      <h3>Share Wrapper</h3>
+      <button onClick={shareFiles}>Share Multiple Files</button>
+      {multipleFileLoading && <div>Loading</div>}
     </div>
   );
 }
