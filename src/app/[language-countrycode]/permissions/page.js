e76638cc4e689ae54109269b2cfe2a1af83ad00c
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-07-30 09:51:11 +0530

    test: dates in google calendar deeplink

--- src/app/[language-countrycode]/permissions/page.js
+++ src/app/[language-countrycode]/permissions/page.js
@@ -276,17 +276,17 @@ export default function Permissions() {
       const res = await AddToCalendar({
         title: "Meeting",
         description: "Meeting with John",
-        // startTime: "2024-06-30T07:35:00Z",
-        // endTime: "2024-06-30T13:41:00Z",
-        // location: {
-        //   name: "Office",
-        //   latitude: 37.7749,
-        //   longitude: -122.4194,
-        // },
-        // allDay: false,
-        // organizer: "tiket",
-        // id: "123456",
-        // remindIn: "5",
+        startTime: 1722407400000,
+        endTime: 1722411000000,
+        location: {
+          name: "Office",
+          latitude: 37.7749,
+          longitude: -122.4194,
+        },
+        allDay: false,
+        organizer: "tiket",
+        id: "123456",
+        remindIn: "5",
       });
     } catch (e) {
       console.log("error", e);
