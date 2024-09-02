f548bcafc10c0d7c7c7bf911706c9f3f961b1073
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-09-02 13:19:42 +0530

    fix: naufal is testing

--- src/app/[language-countrycode]/permissions/addtocalendar/page.js
+++ src/app/[language-countrycode]/permissions/addtocalendar/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { addToCalendar } from "@tiket/react-common-navigator-permission";
 import "./../style.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const CARMapping = {
   android: {
@@ -39,22 +39,6 @@ const CARMapping = {
   },
 };
 
-const defaultEventDetails = {
-  title: "Meeting",
-  description: "Meeting with Vishal",
-  startTime: 1733034600000,
-  endTime: 1733041800000,
-  location: {
-    name: "Office",
-    latitude: 37.7749,
-    longitude: -122.4194,
-  },
-  isAllDay: false,
-  organizer: "vishal.kamra@tiket.com",
-  id: "123456",
-  remindIn: 600,
-};
-
 export default function AddEventToCalendar() {
   const [showSnackBar, setShowSnackBar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
@@ -66,12 +50,12 @@ export default function AddEventToCalendar() {
     }, 4000);
   };
 
-  const addCalendarEvent = async () => {
+  const addCalendarEvent = async (event) => {
     console.log("add-to-calendar entry point");
     try {
       window.CARProperties = CARMapping;
       console.log("add-to-calendar try block");
-      const res = await addToCalendar(defaultEventDetails);
+      const res = await addToCalendar(event);
       console.log("add-to-calendar promise resolved", res);
       setSnackbarMessage(res?.data?.message || "Event added to calendar");
       handleSnackbar();
@@ -84,66 +68,13 @@ export default function AddEventToCalendar() {
     }
   };
 
-  const addAllDayCalendarEvent = async () => {
-    console.log("add-to-calendar entry point");
-    try {
-      window.CARProperties = CARMapping;
-      console.log("add-to-calendar try block");
-      const res = await addToCalendar({
-        ...defaultEventDetails,
-        isAllDay: true,
-      });
-      console.log("add-to-calendar promise resolved", res);
-      setSnackbarMessage(res?.data?.message || "Event added to calendar");
-      handleSnackbar();
-    } catch (e) {
-      console.log("add-to-calendar promise rejected", e);
-      setSnackbarMessage(
-        e?.error?.message || "Failed to add event to calendar"
-      );
-      handleSnackbar();
-    }
-  };
-
-  const failedToAddEvent = async (e) => {
-    console.log("add-to-calendar entry point");
-    try {
-      window.CARProperties = CARMapping;
-      console.log("add-to-calendar try block");
-      const res = await addToCalendar({
-        ...defaultEventDetails,
-        startTime: undefined,
-      });
-      console.log("add-to-calendar promise resolved", res);
-      setSnackbarMessage(res?.data?.message || "Event added to calendar");
-      handleSnackbar();
-    } catch (e) {
-      console.log("add-to-calendar promise rejected", e);
-      setSnackbarMessage(
-        e?.error?.message || "Failed to add event to calendar"
-      );
-      handleSnackbar();
-    }
-  };
+  useEffect(() => {
+    window.addToCalendar = addCalendarEvent;
+  }, []);
 
   return (
     <>
       <h3>Add Events To Calendar</h3>
-      <button onClick={addCalendarEvent}>Add to calendar</button>
-      <button onClick={addAllDayCalendarEvent}>Add to calendar All Day</button>
-      <button onClick={failedToAddEvent}>
-        Failed to add event (only for ios)
-      </button>
-      <span
-        style={{
-          color: "black",
-          textAlign: "center",
-          padding: "5px 0",
-          display: "block",
-        }}
-      >
-        Trying to pass undefined start date to fail the calendar event
-      </span>
       <div className={showSnackBar ? `snackbar show` : `snackbar`}>
         {snackbarMessage}
       </div>
