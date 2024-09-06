0e8d9dea9da3863619d738ec154aef8e3be66d0c
Vishal Kamra <101691491+vishal-tiket@users.noreply.github.com>
2024-09-06 09:04:11 +0530

    fix: throw error on negative remindin timestamp value

--- src/app/[language-countrycode]/maps/page.jsx
+++ src/app/[language-countrycode]/maps/page.jsx
@@ -1,35 +1,102 @@
 "use client";
+import { openMaps } from "@tiket/react-common-navigator-permission";
+import { useState } from "react";
 
-export default function Maps() {
-  const openMapUsingGeo = () => {
-    window.location.href = "geo:0,0?q=37.7749,-122.4194";
+export default function OpenMaps() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [address, setAddress] = useState("");
+  const [gmapsPriority, setGmapsPriority] = useState(false);
+  const [isDirection, setIsDirection] = useState(false);
+
+  const handleOpenMaps = async () => {
+    try {
+      const result = await openMaps({
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+        address,
+        gmapsPriority,
+        isDirection,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
-
-  const openMapsUsingWaze = () => {
-    window.location.href = "waze://?ll=37.7749,-122.4194";
-  };
-
-  const openMapsUsingHereMaps = () => {
-    window.location.href = "here-maps://?map=37.7749,-122.4194";
-  };
-
-  const openMapsUsingMaps = () => {
-    window.location.href = "maps:0,0?q=37.7749,-122.4194";
-  };
-
-  const openMapsUsingComGoogle = () => {
-    window.location.href = "comgooglemaps://?q=37.7749,-122.4194";
-  };
-
   return (
     <>
-      <button onClick={openMapUsingGeo}>Open Maps Using GEO</button>
-      <button onClick={openMapsUsingWaze}>Open Maps Using Waze</button>
-      <button onClick={openMapsUsingHereMaps}>Open Maps Using Here Maps</button>
-      <button onClick={openMapsUsingMaps}>Open Maps Using Maps</button>
-      <button onClick={openMapsUsingComGoogle}>
-        Open Maps Using Com Google
-      </button>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px",
+          fontSize: "20px",
+          marginTop: "40px",
+        }}
+      >
+        <div>
+          <label>Latitude</label>
+          <input
+            type="text"
+            placeholder="latitude"
+            style={{ margin: "10px 0", height: "40px", fontSize: "19px" }}
+            onChange={(e) => setLatitude(e.target.value)}
+            value={latitude}
+          />
+        </div>
+        <div>
+          <label>Longitude</label>
+          <input
+            type="text"
+            placeholder="longitude"
+            style={{ margin: "10px 0", height: "40px", fontSize: "19px" }}
+            onChange={(e) => setLongitude(e.target.value)}
+            value={longitude}
+          />
+        </div>
+        <div>
+          <label>Address</label>
+          <input
+            type="text"
+            placeholder="address"
+            style={{ margin: "10px 0", height: "40px", fontSize: "19px" }}
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <input
+            id="gmapsPriority"
+            type="checkbox"
+            style={{ width: "fit-content", margin: "0 20px 0 0" }}
+            onChange={(e) => setGmapsPriority(e.target.checked)}
+            checked={gmapsPriority}
+          />
+          <label htmlFor="gmapsPriority">Gmaps Priority</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <input
+            id="isDirection"
+            type="checkbox"
+            style={{ width: "fit-content", margin: "0 20px 0 0" }}
+            onChange={(e) => setIsDirection(e.target.checked)}
+            checked={isDirection}
+          />
+          <label htmlFor="isDirection">Is Direction</label>
+        </div>
+      </form>
+      <button onClick={handleOpenMaps}>Open Maps</button>
     </>
   );
 }
